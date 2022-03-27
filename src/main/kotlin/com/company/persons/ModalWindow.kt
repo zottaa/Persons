077420d660ae720b780b58fde6acf2b57ede1054
zottaa <zottaa691@gmail.com>
2022-03-28 00:43:41 +0700

    lab2 not finished

---
+++ src/main/kotlin/com/company/persons/ModalWindow.kt
@@ -0,0 +1,15 @@
+package com.company.persons
+
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.stage.Stage
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.layout.Pane
+import javafx.stage.Modality
+
+
+class ModalWindow {
+
+}
